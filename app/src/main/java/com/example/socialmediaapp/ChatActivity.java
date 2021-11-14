0f1e04cab5b54ed93d680a672c44c16c34baefd2
Hrishikesh kumbhar <hrishikumbhar156@gmail.com>
2021-11-14 19:55:27 +0530

    Chat activity and design changes

--- app/src/main/java/com/example/socialmediaapp/ChatActivity.java
+++ app/src/main/java/com/example/socialmediaapp/ChatActivity.java
@@ -3,17 +3,60 @@ package com.example.socialmediaapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
+import com.example.socialmediaapp.databinding.ActivityChatBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class ChatActivity extends AppCompatActivity {
+    ActivityChatBinding binding;
+    FirebaseDatabase database;
+    ArrayList<User> users;
+    UsersAdapter usersAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        binding =  ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        database = FirebaseDatabase.getInstance();
+        users = new ArrayList<>();
+
+        usersAdapter = new UsersAdapter(this,users);
+        binding.recyclerView.setAdapter(usersAdapter);
+
+
+
+        database.getReference().child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    users.clear();
+                    for(DataSnapshot snapshot1 : snapshot.getChildren()){
+                        User user = snapshot1.getValue(User.class);
+                        users.add(user);
+                    }
+                    usersAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     @Override
